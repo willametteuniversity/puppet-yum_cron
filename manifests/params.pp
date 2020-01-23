45f4377d1fb1fef5ca2f9e6c7ebf4d7295f65d0a
Matthew T Nolley <mnolley@willamette.edu>
2020-01-23 12:11:13 -0800

    NETSRV-7482 # Added params cases for Fedora.

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,30 +7,61 @@ class yum_cron::params {
       $service_hasstatus  = true
       $service_hasrestart = true
 
-      case $::operatingsystemmajrelease {
-        '8': {
-          $package_name     = 'dnf-automatic'
-          $service_name     = 'dnf-automatic.timer'
-          $config_path      = '/etc/dnf/automatic.conf'
-          $debug_level      = '-2'
-          $randomwait       = '360'
+      case $::operatingsystem {
+
+        'RedHat', 'CentOS': {
+          case $::operatingsystemmajrelease {
+            '8': {
+              $package_name     = 'dnf-automatic'
+              $service_name     = 'dnf-automatic.timer'
+              $config_path      = '/etc/dnf/automatic.conf'
+              $debug_level      = '-2'
+              $randomwait       = '360'
+            }
+            '7': {
+              $package_name     = 'yum-cron'
+              $service_name     = 'yum-cron'
+              $config_path      = '/etc/yum/yum-cron.conf'
+              $debug_level      = '-2'
+              $randomwait       = '360'
+            }
+            '6': {
+              $package_name     = 'yum-cron'
+              $service_name     = 'yum-cron'
+              $config_path      = '/etc/sysconfig/yum-cron'
+              $debug_level      = '0'
+              $randomwait       = '60'
+            }
+            default: {
+              fail("Unsupported ${::operatingsystem} operatingsystemmajrelease: ${::operatingsystemmajrelease}, module ${module_name} only supports RedHat 6,7, and 8.")
+            }
+          }
         }
-        '7': {
-          $package_name     = 'yum-cron'
-          $service_name     = 'yum-cron'
-          $config_path      = '/etc/yum/yum-cron.conf'
-          $debug_level      = '-2'
-          $randomwait       = '360'
+
+        'Fedora': {
+          case $::operatingsystemmajrelease {
+            '30', '31', '32': {
+              $package_name     = 'dnf-automatic'
+              $service_name     = 'dnf-automatic.timer'
+              $config_path      = '/etc/dnf/automatic.conf'
+              $debug_level      = '-2'
+              $randomwait       = '360'
+            }
+            '22', '23', '24', '25', '26', '27', '28', '29': {
+              $package_name     = 'yum-cron'
+              $service_name     = 'yum-cron'
+              $config_path      = '/etc/yum/yum-cron.conf'
+              $debug_level      = '-2'
+              $randomwait       = '360'
+            }
+            default: {
+              fail("Unsupported ${::operatingsystem} operatingsystemmajrelease: ${::operatingsystemmajrelease}, module ${module_name} only supports Fedora versions 22-32.")
+            }
+          }
         }
-        '6': {
-          $package_name     = 'yum-cron'
-          $service_name     = 'yum-cron'
-          $config_path      = '/etc/sysconfig/yum-cron'
-          $debug_level      = '0'
-          $randomwait       = '60'
-        }
+
         default: {
-          fail("Unsupported operatingsystemmajrelease: ${::operatingsystemmajrelease}, module ${module_name} only support 6 and 7")
+          fail("Unsupported operatingsystem: ${::operatingsystem}, module ${module_name} only supports RedHat, CentOS, and Fedora.")
         }
       }
     }
